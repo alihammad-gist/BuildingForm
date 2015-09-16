726c5687475e277fcb9c7aa7d1ecbb6ed4a0e610
Ali Hammad <syedalihammadshah@gmail.com>
2015-09-16 09:55:58 +0500

    added configuration for Form/Steps n defaultstep

--- config/module.config.php
+++ config/module.config.php
@@ -1,54 +1,99 @@
 <?php
 return array(
-	'router' => array(
+	'router'        => array(
 		'routes' => array(
 			'building-form' => array(
-				'type' => 'literal',
-				'options' => array(
-					'route' => '/building-form'
-					),
+				'type'          => 'literal',
+				'options'       => array(
+					'route' => '/building-form',
+				),
 				'may_terminate' => false,
-				'child_routes' => array(
-					'home' => array(
-						'type' => 'literal',
+				'child_routes'  => array(
+					'home'   => array(
+						'type'    => 'literal',
 						'options' => array(
-							'route' => '/home',
+							'route'    => '/home',
 							'defaults' => array(
 								'controller' => 'BuildingForm\Index',
-								'action' => 'index',
-								)
-							)
+								'action'     => 'index',
+							),
 						),
-					'cancel' => array(
-						'type' => 'literal',
-						'options' => array(
-							'route' => '/cancel',
-							'defaults' => array(
-								'controller' => 'BuildingForm\Index',
-								'action' => 'cancel',
-								)
-							)
-						)
 					),
-					'complete' => array(
-						'type' => 'literal',
+					'cancel' => array(
+						'type'    => 'literal',
 						'options' => array(
-							'route' => '/complete',
+							'route'    => '/cancel',
 							'defaults' => array(
 								'controller' => 'BuildingForm\Index',
-								'action' => 'complete'
-								)
-							)
-						)
-				)
-			)
+								'action'     => 'cancel',
+							),
+						),
+					),
+				),
+				'complete'      => array(
+					'type'    => 'literal',
+					'options' => array(
+						'route'    => '/complete',
+						'defaults' => array(
+							'controller' => 'BuildingForm\Index',
+							'action'     => 'complete',
+						),
+					),
+				),
+			),
 		),
-	'wizard' => array(
+	),
+	'wizard'        => array(
 		'wizards' => array(
 			'building-form-wizard' => array(
 				'redirect_url' => '/building-form/complete',
-				'cancel_url' => '/building-form/cancel',
-				)
-			)
-		)
+				'cancel_url'   => '/building-form/cancel',
+				'steps'        => array(
+					'BuildingForm\Step\CustomerDetail' => array(
+						'title'         => 'Customer Details',
+						'view_template' => 'building-form/step/default',
+						'form'          => 'BuildingForm\Form\CustomerDetail',
+					),
+					'BuildingForm\Step\BuildingUsage'  => array(
+						'title'         => 'Building Type',
+						'view_template' => 'building-form/step/default',
+						'form'          => 'BuildingForm\Form\BuildingUsage',
+					),
+					'BuildingForm\Step\BuildingType'   => array(
+						'title'         => 'Building Type',
+						'view_template' => 'building-form/step/default',
+						'form'          => 'BuildingForm\Form\BuildingType',
+					),
+					'BuildingForm\Step\BuildingSpec'   => array(
+						'title'         => 'Basic Building Specifications',
+						'view_template' => 'building-form/step/default',
+						'form'          => 'BuildingForm\Form\BuildingSpec',
+					),
+					'BuildingForm\Step\BuildingBay'    => array(
+						'title'         => 'Basic Building Specifications (BAYS)',
+						'view_template' => 'building-form/step/default',
+						'form'          => 'BuildingForm\Form\BuildingBay',
+					),
+				),
+			),
+		),
+	),
+	'form_elements' => array(
+		'invokables' => [
+			'BuildingForm\Form\BuildingBay'    => 'BuildingForm\Form\BuildingBay',
+			'BuildingForm\Form\BuildingType'   => 'BuildingForm\Form\BuildingType',
+			'BuildingForm\Form\BuildingUsage'  => 'BuildingForm\Form\BuildingUsage',
+			'BuildingForm\Form\BuildingSpec'   => 'BuildingForm\Form\BuildingSpec',
+			'BuildingForm\Form\CustomerDetail' => 'BuildingForm\Form\CustomerDetail',
+		],
+	),
+	'wizard_steps'  => array(
+		'invokables' => array(
+			'BuildingForm\Step\CustomerDetail' => 'BuildingForm\DefaultStep',
+			'BuildingForm\Step\BuildingSpec'   => 'BuildingForm\DefaultStep',
+			'BuildingForm\Step\BuildingUsage'  => 'BuildingForm\DefaultStep',
+			'BuildingForm\Step\BuildingType'   => 'BuildingForm\DefaultStep',
+			'BuildingForm\Step\BuildingBay'    => 'BuildingForm\DefaultStep',
+		),
+	),
 );
---
+++ src/BuildingForm/DefaultStep.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BuildingForm;
+
+use Wizard\Step\AbstractStep;
+
+class DefaultStep extends AbstractStep {
+
+}
